16c81b6b94009780daeb38c0ede9be3e7f53c1c8
Cdonaghan <125925805+Cdonaghan@users.noreply.github.com>
2023-12-18 18:13:24 +0000

    Add files via upload

--- PROJECTSEM1.m
+++ PROJECTSEM1.m
@@ -93,3 +93,106 @@ xlabel("time(s)"); % label axes
 ylabel("sample parameters");
 %%attempted to estimate 2 similar peaks then figure out delay between them.
 
+%AI for delay estimation
+% Calculate cross-correlation
+cross_corr = xcorr(x1fsamplesQ3, x2fsamplesQ3);
+% Find the index of the maximum correlation
+[max_corr, max_index] = max(cross_corr);
+% Estimate the relative delay in terms of sample indices
+relative_delay = max_index - length(x1fsamplesQ3);
+fprintf('Relative delay between x1 and x2: %d samples\n', relative_delay);
+%comes out to -1 samples
+
+
+%% Q4
+%For the segments in Question 3, let Xm(ejΩ),
+%m = 1, 2, be their discrete time Fourier transforms. Assuming that X1(ejΩ) 6= 0∀Ω, what would you expect for the
+%quantities
+
+% Calculate Fourier transforms
+X1 = fft(x1fsamplesQ3);
+X2 = fft(x2fsamplesQ3);
+
+% Calculate Gain (G) and Phase (A)
+G = abs(X2 ./ X1);
+A = angle(X2 ./ X1);
+fx1 = (0:(length(X1)-1))/length(X1)*fs;
+fx2 = (0:(length(X2)-1))/length(X2)*fs;
+
+% Plot the results
+figure;
+subplot(2, 1, 1);
+plot(fx1, G);
+title('Gain vs Frequency');
+xlabel('Frequency (\Omega)');
+ylabel('Gain');
+
+subplot(2, 1, 2);
+plot(fx1, A);
+title('Phase vs Frequency');
+xlabel('Frequency (\Omega)');
+ylabel('Phase');
+
+%Display the results for a specific frequency (e.g., at the peak)
+peak_frequency_index = find(G == max(G)); %finding sample number where frequency is highest
+fprintf('Gain at peak frequency: %.4f\n', G(peak_frequency_index)); %gain for sample number 
+fprintf('Phase at peak frequency: %.4f radians\n', A(peak_frequency_index)); %angle for sample number
+%Also could be guessed from graph estimation but this is still the same
+%principle justified
+
+%% Q5
+
+fs = 8000; % sampling frequency in Hertz
+Range = (19501:21548); % window of samples to investigate
+N = length(Range); % number of samples
+X1 = fft(x1(Range)); % Fourier coefficients via fft()
+f = (0:(N-1))/N*fs; % frequency scale
+figure();
+subplot(2, 1, 1);
+plot(f,abs(X1)); % plot magnitude of coefficient
+xlabel('frequency f / [Hz]'); % label axes
+ylabel('magnitude');
+X2 = fft(x2(Range)); % Fourier coefficients via fft()
+subplot(2, 1, 2);
+plot(f,abs(X2)); % plot magnitude of coefficient
+xlabel('frequency f / [Hz]'); % label axes
+ylabel('magnitude');
+
+
+%phase
+phaseQ5 = angle(X2) - angle(X1);
+figure();
+
+plot(f, phaseQ5);
+title('Delay Difference vs Frequency');
+xlabel('Frequency (Hz)');
+ylabel('Delay Difference (radians)');
+
+%The gain and delay difference plots will provide insights into how the signals
+%x1[n] and x2[n]​ differ in terms of magnitude and phase across different frequencies.
+%from mag plot gain diff = 0.5528, taken the peaks and checked y axis
+%difference
+%from angle plot delay estimate = 0.3 radians, 17.1887 degrees
+
+%part 2
+theta1 = angle(X1);
+figure();
+plot(f, theta1);
+title('Phase Angle vs Frequency');
+xlabel('Frequency (Hz)');
+ylabel('Phase Angle (radians)');
+%spikes between 3 and -3 so find 3 radians in degrees = +/- 171.887 degrees
+
+%% Q6
+%The first speaker is quiet during the period 24800 < n ≤ 25824.
+%Use this to estimate the gain and delay difference between x1[n] and x2[n] for speaker 2. W.r.t. Fig. 1, what is the
+%estimated angle ϑ2?
+RangeQ6 = (24800:25824);
+X2 = fft(x2(Range)); % Fourier coefficients via fft()
+theta2 = angle(X2);
+figure();
+plot(f, theta2);
+title('Phase Angle vs Frequency');
+xlabel('Frequency (Hz)');
+ylabel('Phase Angle (radians)');
+%spikes between 3 and -3 so find 3 radians in degrees = +/- 171.887 degrees
